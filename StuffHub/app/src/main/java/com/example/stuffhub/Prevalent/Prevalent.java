81dd26635750697823c24fc51a854ff339716bb7
Lalobeta <45637088+Lalobeta@users.noreply.github.com>
2019-11-06 09:50:50 -0600

    Navigation menu design, login and sign up, admin part

---
+++ StuffHub/app/src/main/java/com/example/stuffhub/Prevalent/Prevalent.java
@@ -0,0 +1,12 @@
+package com.example.stuffhub.Prevalent;
+
+import com.example.stuffhub.Model.Users;
+
+public class Prevalent {
+
+    public static Users currentOnlineUser;
+
+    public static final String UserPhoneKey = "UserPhone";
+    public static final String UserPasswordKey = "UserPassword";
+
+}
